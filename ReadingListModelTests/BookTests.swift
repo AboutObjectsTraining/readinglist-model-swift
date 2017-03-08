cd56f885ce29c77bf2b7731d666b4b6ee545c172
Jonathan Lehr <jonathan@aboutobjects.com>
2017-03-08 15:19:08 -0500

    Initial commit.

---
+++ ReadingListModelTests/BookTests.swift
@@ -0,0 +1,29 @@
+// Copyright (C) 2017 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this example's licensing information.
+//
+import Foundation
+import XCTest
+
+@testable import ReadingListModel
+
+
+let authorDict: [String: Any] = [Author.firstNameKey: NSNull(),
+                                 Author.lastNameKey: "Homer"]
+
+let bookDict: [String: Any] = [Book.titleKey: "My Title",
+                               Book.yearKey: NSNull(),
+                               Book.authorKey: authorDict]
+
+class BookTests: XCTestCase
+{
+    func testInitializeBookWithDictionaryContainingNSNulls() {
+        print(bookDict)
+        let book = Book(dictionary: bookDict)
+        print(book)
+        
+        XCTAssertEqual(book.title, bookDict[Book.titleKey] as? String)
+        XCTAssertEqual(book.year, bookDict[Book.yearKey] as? String)
+        XCTAssertEqual(book.author?.firstName, authorDict[Author.firstNameKey] as? String)
+        XCTAssertEqual(book.author?.lastName, authorDict[Author.lastNameKey] as? String)
+    }
+}
